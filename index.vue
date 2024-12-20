8b6c1d18e8dbc54da7c8e4eaf283cb9daec6d023
ming-lamasherpa <smingma767@gmail.com>
2024-12-20 21:32:54 +0545

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,34 @@
+<template>
+ <v-app>
+    <navbar />
+ <section>
+    <v-carousel
+        show-arrows="hover"
+        cycle>
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+ </section>
+
+  </v-app>
+</template>
+
+<script>
+ 
+</script>
+
+<style lang="scss" scoped>
+
+</style>
